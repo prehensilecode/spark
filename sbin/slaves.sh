fbc40c3d2183a3cea44dec654bbbefe270b67326
David Chin <david.w.h.chin@gmail.com>
2017-10-04 14:47:27 -0400

    Modifications for integration with Univa Grid Engine

--- sbin/slaves.sh
+++ sbin/slaves.sh
@@ -80,20 +80,29 @@ if [ "$HOSTLIST" = "" ]; then
   fi
 fi
 
-
-
 # By default disable strict host key checking
 if [ "$SPARK_SSH_OPTS" = "" ]; then
   SPARK_SSH_OPTS="-o StrictHostKeyChecking=no"
 fi
 
+usershell=$( getent passwd ${USER} | cut -d: -f7 )
 for slave in `echo "$HOSTLIST"|sed  "s/#.*$//;/^$/d"`; do
   if [ -n "${SPARK_SSH_FOREGROUND}" ]; then
-    ssh $SPARK_SSH_OPTS "$slave" $"${@// /\\ }" \
-      2>&1 | sed "s/^/$slave: /"
+    if [ "$usershell" = "/bin/tcsh" -o "$usershell" = "/bin/csh" ]; then
+      ssh $SPARK_SSH_OPTS "$slave" "source ${SPARK_CONF_DIR}/spark-env.csh ;" $"${@// /\\ }" \
+        2>&1 | sed "s/^/$slave: /"
+    else
+      ssh $SPARK_SSH_OPTS "$slave" "source ${SPARK_COFI_DIR}/spark-env.sh ;" $"${@// /\\ }" \
+        2>&1 | sed "s/^/$slave: /"
+    fi
   else
-    ssh $SPARK_SSH_OPTS "$slave" $"${@// /\\ }" \
-      2>&1 | sed "s/^/$slave: /" &
+    if [ "$usershell" = "/bin/tcsh" -o "$usershell" = "/bin/csh" ]; then
+      ssh $SPARK_SSH_OPTS "$slave" "source ${SPARK_CONF_DIR}/spark-env.csh ;" $"${@// /\\ }" \
+        2>&1 | sed "s/^/$slave: /" &
+    else
+      ssh $SPARK_SSH_OPTS "$slave" "source ${SPARK_CONF_DIR}/spark-env.sh ;" $"${@// /\\ }" \
+        2>&1 | sed "s/^/$slave: /" &
+    fi
   fi
   if [ "$SPARK_SLAVE_SLEEP" != "" ]; then
     sleep $SPARK_SLAVE_SLEEP
